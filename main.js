fb1806a7df63110903d34998210fd88480d092f4
Vinizaun <88088847+Vinizaun@users.noreply.github.com>
2024-03-26 12:34:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 //laço de repetição
 
@@ -6,7 +7,30 @@ for(let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
         for (let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-03-26T12:10:00");
+let tempoAtual = new Date();
+
+// contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function atualizarTempo(tempoObjetivo1){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1 - tempoAtual1;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor (segundos / 60);
+    let horas = Math.floor (minutos / 60);
+    let dias = Math.floor (horas / 24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
 }
